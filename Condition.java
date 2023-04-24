14fb882cfd5780910d90d6da954b1ef7f240e170
Bhagyshri1504 <102225187+Bhagyshri1504@users.noreply.github.com>
2023-04-24 20:08:13 +0530

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,16 @@
+public class Condition
+{
+	public static void main(String args[])
+	{
+		int num1=10;
+		int num2=2;
+		if(num1>num2)
+		{
+			System.out.println("Number 1 is greater");
+		}
+		else
+		{
+			System.out.println("Number 2 is greater");
+		}
+	}
+}
